64e67b614138be8a16214eed3c39f6b5f1206e6e
Rafael Sanchez <rafaelesromeru@gmail.com>
2023-06-04 11:02:12 -0500

    feat: :sparkles: Creación de tabla estado y productos

---
+++ database/migrations/2023_06_04_151439_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('product_description');
+            $table->string('product_presentation');
+            $table->string('sku');
+
+            $table->unsignedBigInteger('id_state_fk')->nullable();
+            $table->foreign('id_state_fk')
+                ->references('id')
+                ->on('states')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
