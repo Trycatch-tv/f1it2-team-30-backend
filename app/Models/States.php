524498b56166630ce394f13a9b233ceefd922730
Rafael Sanchez <rafaelesromeru@gmail.com>
2023-06-04 20:21:53 -0500

    feat: :sparkles: Actualiazación de Statates.php y ProductSeeder.php

--- app/Models/States.php
+++ app/Models/States.php
@@ -10,16 +10,3 @@ class States extends Model
     use HasFactory;
     protected $guarded = ['id', 'created_at', 'updated_at'];
 }
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class States extends Model
-{
-    use HasFactory;
-    protected $guarded = ['id', 'created_at', 'updated_at'];
-
-}
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -25,29 +25,3 @@ class ProductSeeder extends Seeder
         ]);
     }
 }
-<?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use App\Models\Products;
-
-class ProductSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Products::create([
-            'product_name' => 'Arroz Diana',
-            'product_description' => 'Arroz Cocina',
-            'product_presentation' => 'bolsa 1kg',
-            'sku' => '1234567890ab',
-                      'id_state_fk' => '1'
-        ]);
-    }
-}
