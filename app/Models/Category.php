5f2b6bd31bc17cca6f11c8f3809b72cdc7a4f764
camiloGarcia12 <122147514+camiloGarcia12@users.noreply.github.com>
2023-06-06 20:23:43 -0500

    feat: commit category

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+
+
+    //Relación de los modelos
+
+    //Relación inversa uno a muchos entre Category-Subcategories
+    public function subcategories(){
+
+        return $this->belongsTo(Subcategories::class);
+    }
+}
