92cbe8a41cc00f76d2cd99f7c3648d6f051c9756
DAlejandro21 <david.acosta.21@outlook.es>
2023-06-08 22:01:35 -0500

    feat(Fusion con juan): :bug:

--- resources/js/Pages/Products.jsx
+++ resources/js/Pages/Products.jsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Link } from '@inertiajs/react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-
-export default function Inicio(props) {
-  return (
-    <>
-      <AuthenticatedLayout
-        auth={props.auth}
-        errors={props.errors}
-        header={<h2 className="font-semibold text-2xl text-black dark:text-[#404750] leading-tight">Lista de Productos</h2>}
-      >
-        <Head title="Products" />
-
-        <div className="py-12">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-              <div className="p-6 text-gray-900 dark:text-gray-100">List of Products ...</div>
-            </div>
-          </div>
-        </div>
-      </AuthenticatedLayout>
-    </>
-  );
-}
-=======
 import React from "react";
 import { Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
@@ -90,4 +62,4 @@ export default function Inicio(props) {
         </>
     );
 }
->>>>>>> 2b7106c675a277836c9d8ec275e083b023f9b86c
+
