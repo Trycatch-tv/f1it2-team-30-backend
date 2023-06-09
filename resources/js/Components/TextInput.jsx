29f6e893d840ff5e7710559b2004188463efe9dc
lexandfr <alexfloresrayme@gmail.com>
2023-06-08 23:18:00 -0500

    feat: :sparkles: add modal form

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -15,7 +15,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
                 {...props}
                 type={type}
                 className={
-                    'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                    'border-lime-500  dark:bg-[#FDEFCE] dark:text-black focus:border-indigo-200 focus:ring-lime-600  rounded-md shadow-sm ' +
                     className
                 }
                 ref={input}
