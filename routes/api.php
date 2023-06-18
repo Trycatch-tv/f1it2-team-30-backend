f64f32de153bf9e09d1278ad17e703b76f15d5e5
Juan Acosta <angelmex121@gmail.com>
2023-06-17 21:03:25 -0400

    feat: :bug: se agrego la ruta de controlador de usuario a api.php en las rutas

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
